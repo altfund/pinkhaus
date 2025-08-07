16b2df78acafac67017db4d8d3b51a6b0408ed7b
gpwclark <gpwclark@gmail.com>
2025-08-07 01:40:55 -0400

    fix proto generation

--- projects/pinkhaus-models/pinkhaus_models/proto/beige_book/transcription_pb2.py
+++ projects/pinkhaus-models/pinkhaus_models/proto/beige_book/transcription_pb2.py
@@ -4,36 +4,39 @@
 # source: transcription.proto
 # Protobuf Python Version: 6.31.1
 """Generated protocol buffer code."""
-
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-
 _runtime_version.ValidateProtobufRuntimeVersion(
-    _runtime_version.Domain.PUBLIC, 6, 31, 1, "", "transcription.proto"
+    _runtime_version.Domain.PUBLIC,
+    6,
+    31,
+    1,
+    '',
+    'transcription.proto'
 )
 # @@protoc_insertion_point(imports)
 
 _sym_db = _symbol_database.Default()
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(
-    b'\n\x13transcription.proto\x12\nbeige_book"9\n\x07Segment\x12\x10\n\x08start_ms\x18\x01 \x01(\x03\x12\x0e\n\x06\x65nd_ms\x18\x02 \x01(\x03\x12\x0c\n\x04text\x18\x03 \x01(\t"\x9a\x01\n\x13TranscriptionResult\x12\x10\n\x08\x66ilename\x18\x01 \x01(\t\x12\x11\n\tfile_hash\x18\x02 \x01(\t\x12\x10\n\x08language\x18\x03 \x01(\t\x12%\n\x08segments\x18\x04 \x03(\x0b\x32\x13.beige_book.Segment\x12\x11\n\tfull_text\x18\x05 \x01(\t\x12\x12\n\ncreated_at\x18\x06 \x01(\x03"f\n\x0c\x46\x65\x65\x64Metadata\x12\x10\n\x08\x66\x65\x65\x64_url\x18\x01 \x01(\t\x12\x0f\n\x07item_id\x18\x02 \x01(\t\x12\r\n\x05title\x18\x03 \x01(\t\x12\x11\n\taudio_url\x18\x04 \x01(\t\x12\x11\n\tpublished\x18\x05 \x01(\t"\x86\x01\n\x1b\x45xtendedTranscriptionResult\x12\x36\n\rtranscription\x18\x01 \x01(\x0b\x32\x1f.beige_book.TranscriptionResult\x12/\n\rfeed_metadata\x18\x02 \x01(\x0b\x32\x18.beige_book.FeedMetadatab\x06proto3'
-)
+
+
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x13transcription.proto\x12\nbeige_book\"9\n\x07Segment\x12\x10\n\x08start_ms\x18\x01 \x01(\x03\x12\x0e\n\x06\x65nd_ms\x18\x02 \x01(\x03\x12\x0c\n\x04text\x18\x03 \x01(\t\"\x9a\x01\n\x13TranscriptionResult\x12\x10\n\x08\x66ilename\x18\x01 \x01(\t\x12\x11\n\tfile_hash\x18\x02 \x01(\t\x12\x10\n\x08language\x18\x03 \x01(\t\x12%\n\x08segments\x18\x04 \x03(\x0b\x32\x13.beige_book.Segment\x12\x11\n\tfull_text\x18\x05 \x01(\t\x12\x12\n\ncreated_at\x18\x06 \x01(\x03\"f\n\x0c\x46\x65\x65\x64Metadata\x12\x10\n\x08\x66\x65\x65\x64_url\x18\x01 \x01(\t\x12\x0f\n\x07item_id\x18\x02 \x01(\t\x12\r\n\x05title\x18\x03 \x01(\t\x12\x11\n\taudio_url\x18\x04 \x01(\t\x12\x11\n\tpublished\x18\x05 \x01(\t\"\x86\x01\n\x1b\x45xtendedTranscriptionResult\x12\x36\n\rtranscription\x18\x01 \x01(\x0b\x32\x1f.beige_book.TranscriptionResult\x12/\n\rfeed_metadata\x18\x02 \x01(\x0b\x32\x18.beige_book.FeedMetadatab\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
-_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, "transcription_pb2", _globals)
+_builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'transcription_pb2', _globals)
 if not _descriptor._USE_C_DESCRIPTORS:
-    DESCRIPTOR._loaded_options = None
-    _globals["_SEGMENT"]._serialized_start = 35
-    _globals["_SEGMENT"]._serialized_end = 92
-    _globals["_TRANSCRIPTIONRESULT"]._serialized_start = 95
-    _globals["_TRANSCRIPTIONRESULT"]._serialized_end = 249
-    _globals["_FEEDMETADATA"]._serialized_start = 251
-    _globals["_FEEDMETADATA"]._serialized_end = 353
-    _globals["_EXTENDEDTRANSCRIPTIONRESULT"]._serialized_start = 356
-    _globals["_EXTENDEDTRANSCRIPTIONRESULT"]._serialized_end = 490
+  DESCRIPTOR._loaded_options = None
+  _globals['_SEGMENT']._serialized_start=35
+  _globals['_SEGMENT']._serialized_end=92
+  _globals['_TRANSCRIPTIONRESULT']._serialized_start=95
+  _globals['_TRANSCRIPTIONRESULT']._serialized_end=249
+  _globals['_FEEDMETADATA']._serialized_start=251
+  _globals['_FEEDMETADATA']._serialized_end=353
+  _globals['_EXTENDEDTRANSCRIPTIONRESULT']._serialized_start=356
+  _globals['_EXTENDEDTRANSCRIPTIONRESULT']._serialized_end=490
 # @@protoc_insertion_point(module_scope)
