adc28426984759334ba3ec67957bbccf528071fa
gpwclark <gpwclark@gmail.com>
2025-08-11 00:02:18 -0400

    proto fix

--- projects/pinkhaus-models/pinkhaus_models/proto/ominari/external_pb2_grpc.py
+++ projects/pinkhaus-models/pinkhaus_models/proto/ominari/external_pb2_grpc.py
@@ -3,7 +3,7 @@
 import grpc
 import warnings
 
-from . import external_pb2 as external__pb2
+import external_pb2 as external__pb2
 
 GRPC_GENERATED_VERSION = '1.74.0'
 GRPC_VERSION = grpc.__version__
